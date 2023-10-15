a5a753df91206c8d23abc464d19bea96790b7311
YuukiIzumi <tnik0f@outlook.com>
2023-10-15 17:19:47 +0800

    Create sy4-2.js

---
+++ javascript/sy4-2.js
@@ -0,0 +1,14 @@
+
+let user=["zhangsan","lisi","hys","wangwu"];
+onmessage=function(event){
+    let result="";
+    username=event.data;
+    //通过event事件来传递参数，参数放在data中
+    //indexof，find，includes
+    if(user.includes(username)){
+        result=username+" 用户名已经存在！"}
+    else{
+        result=username+" 用户名可用"
+    }
+    this.postMessage(result);
+}
